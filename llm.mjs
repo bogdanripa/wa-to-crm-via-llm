cf34825fe12c94c12701ef497c7714eeb7a4fad3
bogdanripa <bogdanripa@gmail.com>
2025-08-17 14:21:34 +0300

    add output item logs

--- llm.mjs
+++ llm.mjs
@@ -150,6 +150,7 @@ export async function getResponseFromLLM(user, from, input, conversationId, shou
             const toolMessages = [];
 
             for (const outputItem of res.output) {
+                console.log(`Processing outputItem: ${JSON.stringify(outputItem, truncateLongStringsReplacer)}`);
                 if (outputItem.type !== "function_call") {
                     console.log("Untreated outputItem type", outputItem.type);
                     continue;
